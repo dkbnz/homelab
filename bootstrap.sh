859c0f75ffcd6388e74b864af76f73183fdc867e
Doug Barrett <mail+git@dkb.nz>
2020-04-08 15:16:13 -0500

    Update bootstrap.sh
    Added docker-compose

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,12 @@ fi
 
 apt-get update && apt-get upgrade
 
-# Install docker
+# Install docker & docker compose
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add --
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 apt-get update
-apt-get install -y docker-ce
+apt-get install -y docker-ce docker-compose
 
 groupadd docker
 usermod -aG docker $USER
